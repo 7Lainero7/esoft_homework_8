a6c533159aed7e8bf9c4563c28906f0f5b84d763
Lainero <7Lainero7@gmail.com>
2025-07-03 13:08:52 +0500

    added test library and script, and productValidator.js

--- validators/productValidator.js
+++ validators/productValidator.js
@@ -15,3 +15,17 @@ exports.productValidationRules = [
     .trim()
     .notEmpty().withMessage('Категория обязательна'),
 ];
+
+const fs = require('fs').promises;
+const path = require('path');
+const filePath = path.join(__dirname, '../data/products.json');
+
+body('name').custom(async (value, { req }) => {
+  const data = await fs.readFile(filePath, 'utf-8');
+  const products = JSON.parse(data || '[]');
+  const exists = products.find(p => p.name === value && p.id !== req.params.id);
+  if (exists) {
+    throw new Error('Продукт с таким названием уже существует');
+  }
+  return true;
+})
